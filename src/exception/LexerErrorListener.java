745c4d08eebdfa77039450dc8cc19cb099ee66e4
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-04-29 01:54:32 -0400

    feat: divisão de erros de lexer e parser para evitar problemas

---
+++ src/exception/LexerErrorListener.java
@@ -0,0 +1,28 @@
+package exception;
+
+
+import org.antlr.v4.runtime.*;
+
+public class LexerErrorListener extends BaseErrorListener {
+ 
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+                            int line, int charPositionInLine, String msg, RecognitionException e) {
+        String lexema = "";
+        if (offendingSymbol instanceof Token token) {
+            lexema = token.getText();
+        } else if (msg.contains("token recognition error")) {
+            // extrai o símbolo inválido da mensagem
+            int start = msg.indexOf("'") + 1;
+            int end = msg.lastIndexOf("'");
+            if (start > 0 && end > start) {
+                lexema = msg.substring(start, end);
+            }
+        }
+
+        String errorMessage = String.format(
+            "ERRO LÉXICO [Linha %d, Coluna %d]: Símbolo '%s' inválido.",
+            line, charPositionInLine + 1, lexema);
+        System.err.println(errorMessage);
+    }
+}
---
+++ src/exception/ParserErrorListener.java
@@ -0,0 +1,41 @@
+package exception;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.IntervalSet;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParserErrorListener extends BaseErrorListener {
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg, RecognitionException e) {
+    
+        String encontrado = offendingSymbol instanceof Token token
+                ? token.getText()
+                : "desconhecido";
+    
+        String esperado = "desconhecido";
+    
+        IntervalSet expectedTokens = null;
+    
+        if (e != null) {
+            expectedTokens = e.getExpectedTokens();
+        } else if (recognizer instanceof Parser parser) {
+            expectedTokens = parser.getExpectedTokens();
+        }
+    
+        if (expectedTokens != null) {
+            List<String> expectedTokenNames = expectedTokens.toList().stream()
+                    .map(tokenType -> recognizer.getVocabulary().getDisplayName(tokenType))
+                    .collect(Collectors.toList());
+            esperado = String.join(" ou ", expectedTokenNames);
+        }
+    
+        System.err.printf("ERRO SINTÁTICO [Linha %d, Coluna %d]: Esperado %s, encontrado '%s'.%n",
+                line, charPositionInLine + 1, esperado, encontrado);
+    }
+    
+}
