b214ce7c65a690c8c595684f3028c2ea08f5a6e5
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-05-26 00:16:05 -0400

    refactor: reorganizado o codigo do main.

--- src/main/java/com/minijava/Main.java
+++ src/main/java/com/minijava/Main.java
@@ -1,7 +1,6 @@
 package com.minijava;
 
 import org.antlr.v4.runtime.*;
-
 import com.minijava.antlr.MiniJavaLexer;
 import com.minijava.antlr.MiniJavaParser;
 import com.minijava.ast.dot.DotGenerator;
@@ -10,100 +9,110 @@ import com.minijava.classcheck.ClassVerification;
 import com.minijava.exception.LexerErrorListener;
 import com.minijava.exception.ParserErrorListener;
 import com.minijava.interpreter.Interpreter;
-import com.minijava.semantic.MiniJavaSemantic;
+import com.minijava.semantic.MiniJavaSemantic; 
 import com.minijava.tokengenerator.TokenGenerator;
 
 import java.io.File;
 import java.io.IOException;
-// import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int option = 0;
-
-        File directory = new File("input/");
-        File[] files = directory.listFiles((dir, name) -> name.endsWith(".txt"));
-
-        if (files != null && files.length > 0) {
-            for (int i = 0; i < files.length; i++) {
-                System.out.println("Arquivo encontrado: " + (i + 1) + "-" + files[i].getName());
-            }
-        } else {
-            System.out.println("Nenhum arquivo encontrado na pasta input.");
-        }
-
-        System.out.println("Digite o numero do arquivo que deseja compilar:");
-        option = scanner.nextInt();
-
-        if (option < 1 || option > files.length) {
-            System.out.println("Escolha inválida.");
-            scanner.close();
-            return;
-        }
-
-        File selectedFile = files[option - 1];
-        System.out.println("Arquivo selecionado: " + selectedFile.getName());
-        System.out.println();
+        File selectedFile = null;
+        Scanner menuScanner = new Scanner(System.in); 
 
         try {
-            // Criação do lexer e parser
-            MiniJavaLexer lexer = new MiniJavaLexer(CharStreams.fromFileName(selectedFile.getPath())); // gera os tokens a partir do codigo fonte
+            int option = 0;
+            File directory = new File("input/");
+            File[] files = directory.listFiles((dir, name) -> name.endsWith(".txt"));
 
-            lexer.removeErrorListeners();
-            lexer.addErrorListener(new LexerErrorListener());
-
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-            MiniJavaParser parser = new MiniJavaParser(tokens); // Consome os tokens do lexer
-            parser.removeErrorListeners();
-            parser.addErrorListener(new ParserErrorListener());
-
-            // Parsing do código
-            MiniJavaParser.ProgramContext tree = parser.program(); // Cria ast
-
-            MiniJavaSemantic semanticAnalyzer = new MiniJavaSemantic();
-            semanticAnalyzer.visit(tree);
-            semanticAnalyzer.printErrors();
-
-            if (!semanticAnalyzer.getSemanticErrors().isEmpty()) {
-                System.out.println("Corrija os erros semanticos antes de continuar");
-                scanner.close();
-                return;
+            if (files != null && files.length > 0) {
+                for (int i = 0; i < files.length; i++) {
+                    System.out.println("Arquivo encontrado: " + (i + 1) + "-" + files[i].getName());
+                }
+            } else {
+                System.out.println("Nenhum arquivo encontrado na pasta input.");
+                menuScanner.close();
+                return; 
             }
 
-            // AstViewer astViewer = new AstViewer(tree, Arrays.asList(parser.getRuleNames()));
-            // astViewer.show();
-
-            // Verificação da classe usando a classe Verification
-            if (!ClassVerification.verifyClassName(tree, selectedFile)) {
-                scanner.close();
+            System.out.println("Digite o numero do arquivo que deseja compilar:");
+            if (menuScanner.hasNextInt()) {
+                option = menuScanner.nextInt();
+            } else {
+                System.out.println("Entrada inválida.");
+                menuScanner.close();
                 return;
             }
+            
+            if (option < 1 || files == null || option > files.length) {
+                System.out.println("Escolha inválida.");
+                menuScanner.close();
+                return;
+            }
+            selectedFile = files[option - 1];
 
-            System.out.println("Parsing e analise semantica completado");
+            System.out.println("Arquivo selecionado: " + selectedFile.getName());
+            System.out.println();
 
-            DotGenerator dotGen = new DotGenerator(tree);
-            dotGen.exportDot("output/dot/ast.dot");
+            try (MiniJavaSemantic semanticAnalyzer = new MiniJavaSemantic()) {
+                MiniJavaLexer lexer = new MiniJavaLexer(CharStreams.fromFileName(selectedFile.getPath()));
+                lexer.removeErrorListeners();
+                LexerErrorListener myLexerErrorListener = new LexerErrorListener();
+                lexer.addErrorListener(myLexerErrorListener);
+                
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                tokens.fill();
+                
+                MiniJavaParser parser = new MiniJavaParser(tokens);
+                parser.removeErrorListeners();
+                ParserErrorListener myParserErrorListener = new ParserErrorListener();
+                parser.addErrorListener(myParserErrorListener); 
+                
+                MiniJavaParser.ProgramContext tree = parser.program();
 
-            // Gera o arquivo png a partir do arquivo dot
-            String dotFilePath = "output/dot/ast.dot";
-            String pngFilePath = "output/dot/pngs/ast.png";
-            AstImageGenerator.generatePngFromDot(dotFilePath, pngFilePath);
+                if (!ClassVerification.verifyClassName(tree, selectedFile)) {
+                    return; 
+                }
 
-            // Interpretador para executar o código
-            Interpreter interpreter = new Interpreter();
-            interpreter.visit(tree);
+                if (parser.getNumberOfSyntaxErrors() > 0 || tree == null) {
+                    System.out.println("Análise abortada devido a erros sintáticos.");
+                    return; 
+                }
 
-            // Criação do arquivo de saída para os tokens
-            TokenGenerator.generate(tokens, "output/tokens/tokens.txt", lexer.getVocabulary());
+                semanticAnalyzer.visit(tree); 
+                semanticAnalyzer.printErrors(); 
 
+                if (!semanticAnalyzer.getSemanticErrors().isEmpty()) {
+                    System.out.println("Corrija os erros semanticos antes de continuar");
+                    return; 
+                }
+
+                // AstViewer astViewer = new AstViewer(tree, Arrays.asList(parser.getRuleNames()));
+                // astViewer.show();
+
+                System.out.println("Parsing e analise semantica completado");
+
+                DotGenerator dotGen = new DotGenerator(tree);
+                dotGen.exportDot("output/dot/ast.dot");
+
+                String dotFilePath = "output/dot/ast.dot";
+                String pngFilePath = "output/dot/pngs/ast.png";
+                AstImageGenerator.generatePngFromDot(dotFilePath, pngFilePath);
+
+                Interpreter interpreter = new Interpreter();
+                interpreter.visit(tree);
+
+                TokenGenerator.generate(tokens, "output/tokens/tokens.txt", lexer.getVocabulary());
+            }
         } catch (IOException e) {
-            System.out.println("Arquivo nao encontrado: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Erro de parsing: " + e.getMessage());
+            System.out.println("Erro de I/O: " + e.getMessage());
+        } catch (Exception e) { 
+            System.out.println("Erro durante o processamento: " + e.getMessage());
+        } finally {
+            if (menuScanner != null) {
+                menuScanner.close();
+            }
         }
-        scanner.close();
     }
 }
