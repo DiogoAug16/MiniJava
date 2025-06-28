2e176f89e237c5f6583a9d499dc7659c3eaa2c7c
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-28 00:31:31 -0400

    refactor: atualizar caminhos de arquivos gerados para a nova estrutura de diretórios

--- src/main/java/com/minijava/antlr/MiniJavaLexer.java
+++ src/main/java/com/minijava/antlr/MiniJavaLexer.java
@@ -1,4 +1,4 @@
-// Generated from src/main/java/com/minijava/grammar/MiniJava.g4 by ANTLR 4.13.1
+// Generated from src/main/java/com/minijava/antlr/MiniJava.g4 by ANTLR 4.13.1
 
 package com.minijava.antlr;
 
