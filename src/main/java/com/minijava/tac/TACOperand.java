4bf4607c2b6360d0f340a5b8292f8325f477a943
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2025-06-27 21:29:48 -0400

    feat: adicionar suporte para tipos literais na classe TACOperand

--- src/main/java/com/minijava/tac/TACOperand.java
+++ src/main/java/com/minijava/tac/TACOperand.java
@@ -1,25 +1,41 @@
 package com.minijava.tac;
 
 public class TACOperand {
+    public enum LiteralType { 
+        INT, STRING 
+    }
+
     public enum Type {
         IDENTIFIER, TEMPORARY, LABEL, LITERAL
     }
+
     private Type type;
     private String value;
+    private LiteralType literalType;
 
     public TACOperand(Type type, String value) {
         this.type = type;
         this.value = value;
     }
-
+    
+    public TACOperand(String value, LiteralType literalType) {
+        this.type = Type.LITERAL;
+        this.value = value;
+        this.literalType = literalType;
+    }
+    
     public Type getType() {
-        return this.type;
+        return type;
     }
-
+    
     public String getValue() {
-        return this.value;
+        return value;
     }
-
+    
+    public LiteralType getLiteralType() {
+        return literalType;
+    }
+    
     @Override
     public String toString() {
         return value;
@@ -28,17 +44,20 @@ public class TACOperand {
     public static TACOperand temporary(int id) {
         return new TACOperand(Type.TEMPORARY, "_t" + id);
     }
-
+    
     public static TACOperand label(int id) {
         return new TACOperand(Type.LABEL, "L" + id);
     }
-
-    public static TACOperand literal(String val) {
-        return new TACOperand(Type.LITERAL, val);
+    
+    public static TACOperand literal(String val, LiteralType type) {
+        return new TACOperand(val, type);
     }
-
+    
+    public static TACOperand literal(String val) {
+        return new TACOperand(val, LiteralType.INT);
+    }
+    
     public static TACOperand identifier(String name) {
         return new TACOperand(Type.IDENTIFIER, name);
     }
-
 }
